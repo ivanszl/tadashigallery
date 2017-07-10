581148c8f7d8f6b0a24bfa2ae15d99baf4e238c9
IvanLam <ivan.lin.1985@gmail.com>
2017-07-10 10:22:16 +0800

    更新支持多个项目并存的情况
    更新支持多个项目并存的情况

--- script/config.lua
+++ script/config.lua
@@ -1,16 +1,17 @@
 local _M = {VERSION = '0.9.8'}
-_M.MYSQL = {
-	host = '127.0.0.1',
-	port = 3306,
-	dbname = 'db_tuku',
-	user = 'test',
-	pwd = '123456'
+_M.default = {
+	MYSQL = {
+		host = '127.0.0.1',
+		port = 3306,
+		dbname = 'db_tuku',
+		user = 'test',
+		pwd = '123456'
+	},
+	TUKU = {
+		user = 'tadashi',
+		password = 'tuku123456',
+		save_path = '/var/www/images'
+	}
 }
 
-_M.TUKU = {
-	user = 'tadashi',
-	password = 'tuku123456',
-	save_path = '/var/www/images'
-}
-
-return _M
+return _M[ngx.var.project_confg]
